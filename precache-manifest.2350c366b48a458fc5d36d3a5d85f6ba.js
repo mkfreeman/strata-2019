90fd4d508b785d54075d71714ba4ac9331d07486
Mike Freeman <michael.k.freeman@gmail.com>
2019-04-26 13:45:09 -0700

    Updates

---
+++ precache-manifest.2350c366b48a458fc5d36d3a5d85f6ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f9f600b1976405a08ac",
+    "url": "/strata-2019/static/js/runtime~main.9f9f600b.js"
+  },
+  {
+    "revision": "81ac3f6ddf16e0517b8a",
+    "url": "/strata-2019/static/js/main.81ac3f6d.chunk.js"
+  },
+  {
+    "revision": "3b734f304f03e20ea84d",
+    "url": "/strata-2019/static/js/1.3b734f30.chunk.js"
+  },
+  {
+    "revision": "81ac3f6ddf16e0517b8a",
+    "url": "/strata-2019/static/css/main.43a861d0.chunk.css"
+  },
+  {
+    "revision": "a3475ae712436cbeb362f56f7a9f423f",
+    "url": "/strata-2019/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/strata-2019/precache-manifest.2350c366b48a458fc5d36d3a5d85f6ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/strata-2019/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
